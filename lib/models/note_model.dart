1c2ddeee1ea4ed943030ee304e1c1dcd52ce1df8
Darshan <127478050+ubdebugger@users.noreply.github.com>
2026-04-01 22:15:49 +0530

    Add files via upload
    changes made in 
    lib/screens/home_screen.dart
    lib/models/note_model.dart
    main.dart

---
+++ lib/models/note_model.dart
@@ -0,0 +1,13 @@
+// lib/models/note_model.dart
+
+class Note {
+  final String title;
+  final String contentPreview;
+  final String timestamp;
+
+  Note({
+    required this.title,
+    required this.contentPreview,
+    required this.timestamp,
+  });
+}
