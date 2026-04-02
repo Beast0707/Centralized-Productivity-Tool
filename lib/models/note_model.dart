b76967b5dd87ae6c873e2b3bb1edc0bb976d3b6c
Darshan <127478050+ubdebugger@users.noreply.github.com>
2026-04-02 09:13:41 +0530

    changed note_model.dart

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,13 +1,1 @@
-// lib/models/note_model.dart
 
-class Note {
-  final String title;
-  final String contentPreview;
-  final String timestamp;
-
-  Note({
-    required this.title,
-    required this.contentPreview,
-    required this.timestamp,
-  });
-}
